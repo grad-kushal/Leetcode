ce674f7cc33123892035f017ac4400334de1e551
grad-kushal <ksk7657@g.rit.edu>
2022-07-10 20:53:11 -0400

    Add first unique character

---
+++ FirstUniqueCharacter/src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String [] args) {
+        System.out.println(findCharFrquencies("dddccdbba"));
+    }
+    public static int findCharFrquencies(String x) {
+        LinkedHashMap<Character, Integer> h = new LinkedHashMap<>();
+        for(int i = 0; i < x.length(); i++) {
+            char currentChar = x.charAt(i);
+            if (!h.containsKey(currentChar))
+                h.put(currentChar, 0);
+            int charFrequency = h.get(currentChar);
+            h.put(currentChar, ++charFrequency);
+        }
+        int index = 0;
+        char answer = '1';
+        for (Map.Entry entry : h.entrySet()) {
+            if ((int) entry.getValue() == 1) {
+                answer = (char) entry.getKey();
+                break;
+            }
+            index++;
+        }
+        return (index == h.size() ? -1 : x.indexOf(answer));
+    }
+}
